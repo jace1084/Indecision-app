31e3cfbb0013486a9d4be030febfcf5018c77285
jace <jace.sf84@gmail.com>
2020-07-16 17:12:04 -0700

    Conditonal rendering and ternery operators

--- client/src/IndecisionApp.js
+++ client/src/IndecisionApp.js
@@ -11,15 +11,19 @@ class IndecisionApp extends Component {
     this.state = {
       isOpen: false,
   }
-console.log("IndecisionApp is running");
+  
 
-}
+};
+ 
+
 
     render() { 
 
       var app = {
         title: "Indecision App",
-        subTitle: "Put your life in the hands of a computer"
+        subTitle: "Put your life in the hands of a computer",
+        options: ["One", "Two"]
+        
       }
 
       var listItems = {
@@ -30,29 +34,39 @@ console.log("IndecisionApp is running");
         var template = (
         <div>
           <h1>{app.title}</h1>
-          <p>{app.subTitle}</p>
+          {app.subTitle && <p>{app.subTitle}</p>}
+          <p>{app.options.length > 0 ? "Here are your options" : "You don't have any Options"}</p>
+          
           <ol>
-            <li className = "listItem">{listItems.itemOne}</li>
-            <li className = "listItem">{listItems.itemTwo}</li>
-            <li className = "listItem">{listItems.itemThree}</li>
+            <li className = "listItem">Item One</li>
+            <li className = "listItem">Item Two</li>
           </ol>
         </div> // This is a Wrapper div  
         );
 
         var user = {
-          name: "Chris Neitro",
-          age: 38,
+          name: "Jason",
+          age: 35,
           location: "San Fracisco"
         };
         // var userName = "Chris Neitro";
         // var userAge = 38;
         // var userLocation = "San Francisco";
 
+       function getLocation(location) {
+          if (location){
+            return <p>Location: {location}</p>;
+          }else{
+            throw Error("Undefined")
+          }
+        };
+
         var templateTwo = (
           <div>
-            <h1>{user.name.toUpperCase() + "!"}</h1>
-            <p>Age:{user.age}</p>
-            <p>Location: {user.location.toUpperCase()}</p>
+            <h1>{user.name ? user.name : "Anon"}</h1>
+            {(user.age >= 18) && <p>Age:{user.age}</p>}
+           
+            {getLocation(user.location.toUpperCase())}
           </div>
         );
 
